2a2b8b2c25201c94d13d8c32f7ec43fd27611439
Andres Rueda <andres.rueda@aheeva.com>
2023-02-16 22:37:19 -0500

    Exercise 1

---
+++ src/exercise1.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    if _first_list == _second_list {
+        Comparison::Equal
+    } else if _first_list.is_empty() {
+        Comparison::Sublist
+    } else if _second_list.is_empty() {
+        Comparison::Superlist
+    } else if _first_list.len() <= _second_list.len() && _second_list.windows(_first_list.len()).any(|w| w == _first_list) {
+        Comparison::Sublist
+    } else if _first_list.len() >= _second_list.len() && _first_list.windows(_second_list.len()).any(|w| w == _second_list) {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
+    }
+
+}
+
+fn main() {
+    let arr: &[u32] = &[];
+    let arr2 :&[u32] = &[1,2,3];
+    println!("{:#?}",sublist(&arr, &arr2));
+}
+
+//https://tndl.medium.com/rusts-slice-windows-is-really-cool-70d50cdc74c5
+// let numbers = vec![1, 2, 3, 4, 5];
+// let has_even = numbers.iter().any(|&x| x % 2 == 0);
+// println!("Has even number: {}", has_even);
+
+// let numbers = vec![1, 2, 3, 4, 5];
+// let any_pair_addition_is_even = numbers.windows(2).any(|slice| {
+//         match slice {
+//             [a, b] => (a + b) % 2 == 0,
+//             _ => false,  // other slice lengths not allowed
+//         }
+//     });
+// println!("Has even number: {:#?}", any_pair_addition_is_even);
